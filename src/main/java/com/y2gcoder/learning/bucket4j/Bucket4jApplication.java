573676d1731ead9bed2603e33f6c656e0423a4b2
y2gcoder <y2gcoder@gmail.com>
2023-09-19 11:27:27 +0900

    first commit

---
+++ src/main/java/com/y2gcoder/learning/bucket4j/Bucket4jApplication.java
@@ -0,0 +1,13 @@
+package com.y2gcoder.learning.bucket4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bucket4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bucket4jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/y2gcoder/learning/bucket4j/Bucket4jApplicationTests.java
@@ -0,0 +1,13 @@
+package com.y2gcoder.learning.bucket4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bucket4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
